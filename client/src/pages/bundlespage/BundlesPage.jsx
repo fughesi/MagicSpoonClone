bd5240aa318bde1a96d4bfe2b9597e651993cb08
Steven <steven.camillieri@protonmail.com>
2023-03-06 07:00:15 -0500

    finishing bundle page

--- client/src/pages/bundlespage/BundlesPage.jsx
+++
@@ -1,7 +0,0 @@
-import "./BundlesPage.css";
-
-export default function BundlesPage() {
-  const content = <main className="bundlesPageContainer">BundlesPage</main>;
-
-  return content;
-}
--- client/src/pages/productpage/ProductPage.jsx
+++ client/src/pages/productpage/ProductPage.jsx
@@ -1,3 +1,4 @@
+import { useRef, useState, useEffect } from "react";
 import {
   img1,
   img2,
@@ -16,6 +17,26 @@ import "./ProductPage.css";
 export default function ProductPage({ type, style }) {
   const productPhotoArray = [img1, img2, img3, img4, img5, img6, img7, img8];
 
+  const [observer, setObserver] = useState("");
+  const rotateTarget = useRef();
+
+  useEffect(() => {
+    const currentTarget = rotateTarget.current;
+
+    const rotateIO = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((e) => {
+          if (e.isIntersecting) setObserver("rotationalObserver");
+        });
+      },
+      { root: null, margin: "0px", threshold: 0.3 }
+    );
+
+    if (currentTarget) rotateIO.observe(currentTarget);
+
+    return rotateIO.unobserve(currentTarget);
+  }, [rotateTarget]);
+
   const content = (
     <main className="productPageContainer">
       <section className="productPageSection1">
@@ -29,9 +50,59 @@ export default function ProductPage({ type, style }) {
         </div>
       </section>
 
+      <section className="productPageSection2">
+        <div className="productCartDiv">
+          <h3>CEREAL & BARS COMBO BUNDLE</h3>
+          <h4>4 CEREAL BOXES + 8 CEREAL BARS</h4>
+          <pre>⭐️⭐️⭐️⭐️⭐️ 39261 Reviews</pre>
+          <p>
+            Get the best of both worlds—10g of protein in your pocket, and the nostalgic flavors of your childhood
+            mornings back in your bowl. Take our cereal bars wherever you go with two classic flavors, Cookies & Cream
+            and Cocoa Peanut Butter, and enjoy our cereal in four bestselling flavors.
+          </p>
+          <div className="productSelectionDiv">
+            <label htmlFor="productSelect">
+              CHOOSE FLAVORS <br />
+            </label>
+            <select name="productSelect" id="productSelect" type="select">
+              <option value="" selected>
+                CEREAL BARS AND COMBO BUNDLES &nbsp;&nbsp;&nbsp; ➢
+              </option>
+              <optgroup label="best-sellers">
+                <option value="berry">BERRY </option>
+                <option value="fruity">FRUITY</option>
+                <option value="pebbles">PEBBLES</option>
+              </optgroup>
+              <optgroup label="nastiest-tasting">
+                <option value="grapefruit">GRAPEFRUIT</option>
+                <option value="banana">BANANA</option>
+              </optgroup>
+            </select>
+            <br />
+            <p>
+              <span className="priceCut">$59</span>&nbsp; $54
+            </p>
+
+            <div className="addQuantityCart">
+              <div>
+                <p>QUANTITY</p>
+                <p>
+                  <span onClick={() => console.log("clicked")}> &lt; </span>
+                  {"0"} <span onClick={() => console.log("clicked")}> &gt;</span>
+                </p>
+              </div>
+              <div>
+                <p onClick={() => console.log("clicked")}>ADD TO CART</p>
+              </div>
+            </div>
+          </div>
+          <p id="tryRiskFree">Try risk-free, 100% happiness guaranteed. See terms of use.</p>
+        </div>
+      </section>
+
       <section className="productPageSection3">
         <div className="rotatingProductBackground">
-          <img src={barzz} alt="rotating bar" />
+          <img src={barzz} alt="rotating bar" className={observer} ref={rotateTarget} />
         </div>
 
         <div className="productInformationGrid">
